5466707d74729457c6694462921a78d250ac6527
John Knotts <jhnnyk@gmail.com>
2016-03-03 07:04:43 -0700

    improved join with joinor method

--- lesson_4/tictactoe.rb
+++ lesson_4/tictactoe.rb
@@ -11,6 +11,11 @@ def prompt(msg)
   puts "=> #{msg}"
 end
 
+def joinor(array, delimeter=', ', word='or')
+  array[-1] = "#{word} #{array.last}" if array.size > 1
+  array.join(delimeter)
+end
+
 # rubocop:disable Metrics/AbcSize
 def display_board(brd)
   system 'clear'
@@ -44,7 +49,7 @@ end
 def player_places_piece!(brd)
   square = ''
   loop do
-    prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
+    prompt "Choose a square (#{joinor(empty_squares(brd))}):"
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
     prompt "Sorry, that's not a valid choice."
