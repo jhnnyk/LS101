b6fb0b14392885858a4e1ad576036eab2341e741
John Knotts <jhnnyk@gmail.com>
2016-03-04 18:12:17 -0700

    Computer AI defense

--- lesson_4/tictactoe.rb
+++ lesson_4/tictactoe.rb
@@ -47,6 +47,14 @@ def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
+def find_at_risk_square(line, brd)
+  if brd.values_at(*line).count(PLAYER_MARKER) == 2
+    brd.select{ |k, v| line.include?(k) && v == INITIAL_MARKER}.keys.first
+  else
+    nil
+  end
+end
+
 def player_places_piece!(brd)
   square = ''
   loop do
@@ -60,7 +68,16 @@ def player_places_piece!(brd)
 end
 
 def computer_places_piece!(brd)
-  square = empty_squares(brd).sample
+  square = nil
+  WINNING_LINES.each do |line|
+    square = find_at_risk_square(line, brd)
+    break if square
+  end
+
+  if !square
+    square = empty_squares(brd).sample
+  end
+
   brd[square] = COMPUTER_MARKER
 end
 
