0d43254ded8137d56f4925cae7b0bd7686871076
John Knotts <jhnnyk@gmail.com>
2016-03-07 13:43:09 -0700

    destructive on hashes, but not variables?

--- lesson_4/scope_test.rb
+++ lesson_4/scope_test.rb
@@ -1,17 +1,44 @@
-# player_score = 0
-# computer_score = 0
+player_score = 5
+computer_score = 7
 
 def keep_score(player_score, computer_score)
   player_score += 1
   computer_score += 1
-  puts "Player score id (inside the method): #{player_score.object_id}"
-  puts "Computer score id (inside the method): #{computer_score.object_id}"
   return player_score, computer_score
 end
 
-# puts "Player score is: #{player_score}"
-# puts "Player score id (outside the method): #{player_score.object_id}"
-# puts "Computer score is: #{computer_score}"
-# puts "Computer score id (outside the method): #{computer_score.object_id}"
+p keep_score(player_score, computer_score)
+p player_score
+p computer_score
 
-p keep_score(5, 10)
+# --------------------
+
+score = { player: 5, computer: 7 }
+
+def another_score(score)
+  score[:player] += 1
+  score[:computer] += 1
+  score
+end
+
+p another_score(score)
+p score
+
+# --------------------
+
+# v0 = 0
+
+# class SomeClass # Scope gate
+#   v1 = 1
+#   p local_variables
+
+#   def some_method # Scope gate
+#     v2 = 2
+#     p local_variables
+#   end # end of def scope gate
+# end # end of class scope gate
+
+# some_class = SomeClass.new
+# some_class.some_method
+
+# p local_variables
