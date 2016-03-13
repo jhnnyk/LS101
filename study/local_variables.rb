d62d6c8373b41c2a4af3595857b791e7b0787d0d
John Knotts <jhnnyk@gmail.com>
2016-03-13 09:37:24 -0600

    study more

--- study/local_variables.rb
+++ study/local_variables.rb
@@ -40,13 +40,37 @@
 #   puts str2       # and they still have access to variables in the outer scope
 # end
 
-str2 = 'world'
+# str2 = 'world'
 
-[1].each do |n|
-  puts str2
-  str2 = "a new string"
+# [1].each do |n|
+#   puts str2
+#   str2 = "a new string"
+# end
+
+# puts str2     # this will print out 'a new string' because within a block
+#               # you can reassign and modify outer scope variables
+
+# def str2
+#   "from method"
+# end
+
+# [1].each do |n|
+#   puts str2
+#   str2 = "a new string"
+# end
+
+# puts str2
+
+
+# ---------------
+# method args
+
+def run(str)
+  str += ' world'
+  str << ' world'
 end
 
-puts str2     # this will print out 'a new string' because within a block
-              # you can reassign and modify outer scope variables
+a = "hello"
+run(a)
 
+puts a
