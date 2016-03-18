4e18ac1f6695026e8d873521ec20166d0504e511
John Knotts <jhnnyk@gmail.com>
2016-03-18 17:47:22 -0600

    refactor reverse an array without Array#reverse method

--- study/reverse.rb
+++ study/reverse.rb
@@ -1,13 +1,17 @@
-arr = ['one', 'two', 'three', 'four', 'five']
+def reverse_array(arr)
+  reverse_arr = []
 
-reverse_arr = []
+  i = 0
+  loop do
+    reverse_arr[i] = arr[arr.size - 1 - i]
+    i += 1
+    break if i == arr.size
+  end
 
-i = 0
-loop do
-  reverse_arr[i] = arr[arr.size - i - 1]
-  break if i == arr.size - 1
-  i += 1
+  reverse_arr
 end
 
+arr = ['one', 'two', 'three', 'four', 'five', 'six']
+
 p arr
-p reverse_arr
+p reverse_array(arr)
