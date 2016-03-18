3c1814d15adbab1670ca7398d7aa562e2727e593
John Knotts <jhnnyk@gmail.com>
2016-03-18 17:09:02 -0600

    refactor remove vowels project

--- study/interview.rb
+++ study/interview.rb
@@ -1,23 +1,15 @@
 VOWELS = ['a', 'e', 'i', 'o', 'u']
 
-def remove_vowels(str)
-  word_arr = str.split("")
-  VOWELS.each do |char|
-    word_arr.delete(char)
+def remove_vowels(arr)
+  arr.map do |str|
+    chars = str.split("")
+    VOWELS.each do |vowel|
+      chars.delete(vowel)
+    end
+    chars.join("")
   end
-  no_vowels = word_arr.join("")
-end
-
-def remove_vowels_arr(arr)
-  no_vowels_arr = []
-  
-  arr.each do |word|
-    no_vowels_arr << remove_vowels(word)
-  end
-
-  no_vowels_arr
 end
 
 arr = ['green', 'yellow', 'black', 'white']
 
-p remove_vowels_arr(arr)
+p remove_vowels(arr)
