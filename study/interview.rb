d10eb4c7fc2c3574c056d7d69c99fa4fe1f47de3
John Knotts <jhnnyk@gmail.com>
2016-03-18 16:29:00 -0600

    product search

--- study/interview.rb
+++ study/interview.rb
@@ -1,19 +1,45 @@
-def fizzbuzz(num1, num2)
-  fb_arr = []
+PRODUCTS = [
+  { name: "Thinkpad x210", price: 220 },
+  { name: "Thinkpad x220", price: 250 },
+  { name: "Thinkpad x250", price: 979 },
+  { name: "Thinkpad x230", price: 300 },
+  { name: "Thinkpad x230", price: 330 },
+  { name: "Thinkpad x230", price: 350 },
+  { name: "Thinkpad x240", price: 700 },
+  { name: "Macbook Leopard", price: 300 },
+  { name: "Macbook Air", price: 700 },
+  { name: "Macbook Pro", price: 600 },
+  { name: "Macbook", price: 1449 },
+  { name: "Dell Latitude", price: 200 },
+  { name: "Dell Latitude", price: 650 },
+  { name: "Dell Inspiron", price: 300 },
+  { name: "Dell Inspiron", price: 450 },
+]
 
-  (num1..num2).each do |num|
-    if num%15 == 0
-      fb_arr << "FizzBuzz"
-    elsif num%5 == 0
-      fb_arr << "Buzz"
-    elsif num%3 == 0
-      fb_arr << "Fizz"
-    else
-      fb_arr << num
-    end
+query = {
+  price_min: 240,
+  price_max: 280,
+  q: "thinkpad"
+}
+
+query2 = {
+  price_min: 300,
+  price_max: 450,
+  q: 'dell'
+}
+
+def search(query)
+  # implementation goes here
+  PRODUCTS.select do |product|
+    product[:price] >= query[:price_min] &&
+    product[:price] <= query[:price_max] &&
+    product[:name].downcase.include?(query[:q])
   end
-
-  fb_arr
 end
 
-p fizzbuzz(1, 15)
+p search(query)
+# [ { name: "Thinkpad x220", price: 250 } ]
+
+p search(query2)
+# [{ name: "Dell Inspiron", price: 300 },
+#  { name: "Dell Inspiron", price: 450 }]
