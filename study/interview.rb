1d10153f0653555d82a00cd3b62d62f2db8cf03f
John Knotts <jhnnyk@gmail.com>
2016-03-17 18:06:09 -0600

    reverse array without using Array#reverse

--- study/interview.rb
+++ study/interview.rb
@@ -1,7 +1,15 @@
 def reverse_array(arr)
-  
+  rev_arr = []
+  i = 0
+  loop do |str|
+    rev_arr[arr.size - 1 - i] = arr[i]
+    i += 1
+    break if i == arr.size
+  end
+  rev_arr
 end
 
-arr = ['one', 'two', 'three', 'four', 'five']
-rev_arr = []
+arr = ['one', 'two', 'three', 'four', 'five', 'six']
+
+p reverse_array(arr)
 
