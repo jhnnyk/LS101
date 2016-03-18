033b47ca4477c434bab44cbec4066f2620dfe24b
John Knotts <jhnnyk@gmail.com>
2016-03-18 15:49:47 -0600

    select element if its index is a fibonacci number

--- study/interview.rb
+++ study/interview.rb
@@ -1,13 +1,24 @@
-def reverse_string(string)
-  string_arr = string.split("")
-  rev_string_arr = []
+def fibonacci(size)
+  fib = [0, 1]
 
-  string.size.times do |letter|
-    rev_string_arr << string_arr.pop
+  loop do
+    fib[fib.size] = fib[fib.size - 1] + fib[fib.size - 2]
+    break if fib.size == size
   end
-  rev_string = rev_string_arr.join("")
-
-  rev_string
+  fib
 end
 
-p reverse_string("hello")
+def fib_elements(arr)
+  fib_arr = []
+  fib_nums = fibonacci(arr.size)
+
+  arr.each_with_index do |item, index|
+    fib_arr << item if fib_nums.include?(index)
+  end
+  
+  fib_arr
+end
+
+arr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight']
+
+p fib_elements(arr)
