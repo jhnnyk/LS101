4f0c4fa26705eaa58ab9a83e0c2d7f797d0d522a
John Knotts <jhnnyk@gmail.com>
2016-03-19 15:47:45 -0600

    select element out of an array if its index is a fibonacci number

--- study/interview.rb
+++ study/interview.rb
@@ -1,16 +1,26 @@
-def reverse_arr(arr)
-  rev_arr = []
+def fibonacci_numbers(arr_size)
+  fib_nums = [0, 1]
 
-  i = 0
   loop do
-    rev_arr[arr.size - 1 - i] = arr[i]
-    i += 1
-    break if i == arr.size
+    fib_nums[fib_nums.size] = fib_nums[fib_nums.size - 1] + fib_nums[fib_nums.size - 2]
+    break if fib_nums.size == arr_size
   end
 
-  rev_arr
+  fib_nums
 end
 
-arr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six']
+def select_if_fib_index(arr)
+  fib_index = fibonacci_numbers(arr.size)
+  fib_arr = []
 
-p reverse_arr(arr)
+  arr.each_with_index do |item, index|
+    fib_arr << item if fib_index.include?(index)
+  end
+
+  fib_arr
+end
+
+arr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight']
+
+p select_if_fib_index(arr)
+
