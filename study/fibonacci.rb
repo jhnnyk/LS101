5a8c30668a81c8bad84db9eca6bf93f80a1cd465
John Knotts <jhnnyk@gmail.com>
2016-03-18 18:19:45 -0600

    refactor select element if its index is a fibonacci number

--- study/fibonacci.rb
+++ study/fibonacci.rb
@@ -1,22 +1,25 @@
-def fibonacci(index)
+def fibonacci(num)
   fib = [0, 1]
+
   loop do
     fib[fib.size] = fib[fib.size - 1] + fib[fib.size - 2]
-    break if fib.size >= index
+    break if fib.size >= num
   end
+
   fib
 end
 
 def select_if_fib(arr)
-  new_arr = []
-  fibonacci_sequence = fibonacci(arr.size)
-  index_arr = arr.each_index.select { |i| fibonacci_sequence.include?(i) }
-  index_arr.each do |n|
-    new_arr << arr[n]
+  fibonacci_numbers = fibonacci(arr.size)
+  fib_arr = []
+
+  arr.each_with_index do |item, index|
+    fib_arr << item if fibonacci_numbers.include?(index)
   end
-  new_arr
+
+  fib_arr
 end
 
-arr = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+arr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
 
 p select_if_fib(arr)
