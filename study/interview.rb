fb73611739861067006fc4bcee3b020efbda5608
John Knotts <jhnnyk@gmail.com>
2016-03-19 19:11:36 -0600

    practice find_primes problem again

--- study/interview.rb
+++ study/interview.rb
@@ -1,18 +1,15 @@
-def balancer(str)
-  return false unless str.count("(") == str.count(")")
-
-  paren_count = 0
-  str_arr = str.split("")
-  str_arr.each do |letter|
-    paren_count += 1 if letter == "("
-    paren_count -= 1 if letter == ")"
-    return false if paren_count < 0
+def is_prime?(num)
+  (2..(num - 1)).each do |divisor|
+    return false if num % divisor == 0
   end
 
   true
 end
 
-p balancer("hi")    # => true
-p balancer("(hi")   # => false
-p balancer("(hi)")  # => true
-p balancer(")hi(")  # => false
+def find_primes(num1, num2)
+  (num1..num2).select do |number|
+    number if is_prime?(number)
+  end
+end
+
+p find_primes(3, 10)
