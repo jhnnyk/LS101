04ddcedb5a72b4c9471e447774b36129e6899552
John Knotts <jhnnyk@gmail.com>
2016-03-22 16:59:12 -0600

    add guess count

--- study/guess_number.rb
+++ study/guess_number.rb
@@ -2,6 +2,7 @@ computer_number = (1..100).to_a.sample
 
 puts "Please guess a number between 1 & 100:"
 guess = gets.chomp.to_i
+guess_count = 1
 
 loop do
   if computer_number < guess
@@ -15,7 +16,12 @@ loop do
 
   puts "Please guess again:"
   guess = gets.chomp.to_i
+  guess_count += 1
 end
 
+puts "---"
 p computer_number
 p guess
+puts "---"
+
+puts "It took #{guess_count} guesses!"
