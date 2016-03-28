debb3ec5469c4ae30411f26591ea6fe0f18405b5
John Knotts <jhnnyk@gmail.com>
2016-03-28 13:17:27 -0600

    Is this my tail? Kata

--- study/kata.rb
+++ study/kata.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Strive Matching # 2
 # http://www.codewars.com/kata/56c2578be8b139bd5c001bd8/train/ruby
 
@@ -49,14 +50,27 @@
 # -------------------
 # My head is at the wrong end!
 # http://www.codewars.com/kata/56f699cd9400f5b7d8000b55/train/ruby
-def fix_the_meerkat(arr)
-  #your code here
+# def fix_the_meerkat(arr)
+#   arr.reverse
+# end
+
+# p fix_the_meerkat(["tail", "body", "head"]) # => ["head", "body", "tail"]
+# p fix_the_meerkat(["tails", "body", "heads"]) # => ["heads", "body", "tails"]
+# p fix_the_meerkat(["bottom", "middle", "top"]) # => ["top", "middle", "bottom"]
+# p fix_the_meerkat(["lower legs", "torso", "upper legs"]) # => ["upper legs", "torso", "lower legs"]
+# p fix_the_meerkat(["ground", "rainbow", "sky"]) # => ["sky", "rainbow", "ground"]
+
+
+# ---------------------
+# Is this my tail?
+# http://www.codewars.com/kata/56f695399400f5d9ef000af5/train/ruby
+def correct_tail(body, tail)
+  body.chars.last == tail
 end
 
-fix_the_meerkat(["tail", "body", "head"]) # => ["head", "body", "tail"]
-fix_the_meerkat(["tails", "body", "heads"]) # => ["heads", "body", "tails"]
-fix_the_meerkat(["bottom", "middle", "top"]) # => ["top", "middle", "bottom"]
-fix_the_meerkat(["lower legs", "torso", "upper legs"]) # => ["upper legs", "torso", "lower legs"]
-fix_the_meerkat(["ground", "rainbow", "sky"]) # => ["sky", "rainbow", "ground"]
-
-
+p correct_tail("Fox", "x") # => true
+p correct_tail("Rhino", "o") # => true
+p correct_tail("Meerkat", "t") # => true
+p correct_tail("Emu", "t") # => false
+p correct_tail("Badger", "s") # => false
+p correct_tail("Giraffe", "d") # => false
