78bc03da6e9c1c4b8c5c607add6feec0f7b76972
John Knotts <jhnnyk@gmail.com>
2016-03-29 20:29:07 -0600

    study: fib index array problem

--- study/interview.rb
+++ study/interview.rb
@@ -1,15 +1,26 @@
-def reverse_array(arr)
-  rev_arr = []
+def fibonacci_numbers(n)
+  fib = [0, 1]
 
-  i = 0
-  arr.size.times do
-    rev_arr << arr[arr.size - 1 - i]
-    i += 1
+  loop do
+    fib[fib.size] = fib[fib.size - 1] + fib[fib.size - 2]
+    break if fib.size == n
   end
 
-  rev_arr
+  fib
+end
+
+def has_fib_index(arr)
+  fib_arr = []
+
+  arr.each_with_index do |item, index|
+    if fibonacci_numbers(arr.size).include?(index)
+      fib_arr << item
+    end
+  end
+
+  fib_arr
 end
 
 arr = ['zero', 'one', 'two', 'three', 'four', 'five']
 
-p reverse_array(arr)
+p has_fib_index(arr)
